d79ee0d0a9af938d16ac60f3bc15d262e53504eb
Jeandre Visser <jeandre@nutrimeals.ca>
2023-06-18 16:20:43 -0600

    Map over car cards in the home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,14 @@
 import CustomFilter from '@/components/CustomFilter';
 import Hero from '@/components/Hero';
 import SearchBar from '@/components/SearchBar';
+import { fetchCars } from '@/utils';
 import Image from 'next/image';
 
-export default function Home() {
+export default async function Home() {
+  const allCars = await fetchCars();
+
+  const isDataEmpty = !Array.isArray(allCars) || allCars.length < 1 || !allCars;
+
   return (
     <main className='overflow-hidden'>
       <Hero />
@@ -17,10 +22,25 @@ export default function Home() {
           <SearchBar />
 
           <div className='home__filter-container'>
-            <CustomFilter title='fuel' />
-            <CustomFilter title='year' />
+            {/* <CustomFilter title='fuel' />
+            <CustomFilter title='year' /> */}
           </div>
         </div>
+
+        {!isDataEmpty ? (
+          <section>
+            <div className='home__card-wrapper'>
+              {allCars?.map((car) => (
+                <CarCard car={car} />
+              ))}
+            </div>
+          </section>
+        ) : (
+          <div className='home__error-container'>
+            <h2 className='text-black text-xl font-bold'>Oops, no results</h2>
+            <p>{allCars?.message}</p>
+          </div>
+        )}
       </div>
     </main>
   );
--- utils/index.ts
+++ utils/index.ts
@@ -14,3 +14,20 @@ try {
 } catch (error) {
   console.error(error);
 }
+
+export async function fetchCars() {
+  const headers = {
+    'X-RapidAPI-Key': 'f68de90bc2msh0afc2ba5c8e7022p11c4fejsn457033c3b522',
+    'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com',
+  };
+
+  const response = await fetch(
+    'https://cars-by-api-ninjas.p.rapidapi.com/v1/cars',
+    {
+      headers: headers,
+    }
+  );
+  const result = await response.json();
+
+  return result;
+}
