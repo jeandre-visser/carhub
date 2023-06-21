e9732f5dbccbabc17b42fe443c8875809876e2d3
Jeandre Visser <jeandre@nutrimeals.ca>
2023-06-20 21:08:27 -0600

    Update main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,95 +1,87 @@
-'use client';
+"use client";
 
-import CarCard from '@/components/CarCard';
-import CustomFilter from '@/components/CustomFilter';
-import Hero from '@/components/Hero';
-import SearchBar from '@/components/SearchBar';
-import ShowMore from '@/components/ShowMore';
-import { fuels, yearsOfProduction } from '@/constants';
-import { fetchCars } from '@/utils';
-import Image from 'next/image';
-import { useEffect, useState } from 'react';
+import { useState, useEffect } from "react";
+import Image from "next/image";
+
+import { fetchCars } from "@/utils";
+import { fuels, yearsOfProduction } from "@/constants";
+import CarCard from "@/components/CarCard";
+import ShowMore from "@/components/ShowMore";
+import SearchBar from "@/components/SearchBar";
+import CustomFilter from "@/components/CustomFilter";
+import Hero from "@/components/Hero";
+import { CarState } from "@/types";
 
 export default function Home() {
-  const [allCars, setAllCars] = useState([]);
+  const [allCars, setAllCars] = useState<CarState>([]);
   const [loading, setLoading] = useState(false);
 
-  const [manufacturer, setManuFacturer] = useState('');
-  const [model, setModel] = useState(''); 
+  // search states
+  const [manufacturer, setManuFacturer] = useState("");
+  const [model, setModel] = useState("");
+
+  // filter state
+  const [fuel, setFuel] = useState("");
   const [year, setYear] = useState(2022);
-  const [fuel, setFuel] = useState('');
+
+  // limit state
   const [limit, setLimit] = useState(10);
 
   const getCars = async () => {
-    setLoading(true)
-
+    setLoading(true);
     try {
-      const result = await fetchCars({ 
-      manufacturer: manufacturer || '',
-      year: year || 2022,
-      fuel: fuel || '',
-      limit: limit || 10,
-      model: model || '',
-    });
+      const result = await fetchCars({
+        manufacturer: manufacturer.toLowerCase() || "",
+        model: model.toLowerCase() || "",
+        fuel: fuel.toLowerCase() || "",
+        year: year || 2022,
+        limit: limit || 10,
+      });
+
       setAllCars(result);
-    } catch (error) {
-      console.log(error)
+    } catch {
+      console.error();
     } finally {
-      setLoading(false)
+      setLoading(false);
     }
-}
+  };
 
   useEffect(() => {
-    getCars()
-  }, [
-    manufacturer,
-    model,
-    year,
-    fuel,
-    limit
-  ])
-
+    getCars();
+  }, [fuel, year, limit, manufacturer, model]);
 
   return (
     <main className='overflow-hidden'>
       <Hero />
+
       <div className='mt-12 padding-x padding-y max-width' id='discover'>
         <div className='home__text-container'>
-          <h1 className='text-4xl font-extrabold'>Car Catelogue</h1>
-          <p>Explore the cars you might like</p>
+          <h1 className='text-4xl font-extrabold'>Car Catalogue</h1>
+          <p>Explore out cars you might like</p>
         </div>
 
         <div className='home__filters'>
-          <SearchBar 
-            setManuFacturer={setManuFacturer}
-            setModel={setModel}
-          />
+          <SearchBar setManuFacturer={setManuFacturer} setModel={setModel} />
 
           <div className='home__filter-container'>
-            <CustomFilter title='fuel' options={fuels}
-              setFilter={setFuel}
-            />
-            <CustomFilter title='year'  options={yearsOfProduction}
-              setFilter={setYear}
-            />
+            <CustomFilter options={fuels} setFilter={setFuel} />
+            <CustomFilter options={yearsOfProduction} setFilter={setYear} />
           </div>
         </div>
 
         {allCars.length > 0 ? (
           <section>
             <div className='home__cars-wrapper'>
-              {allCars?.map((car) => (
-                <CarCard car={car} />
+              {allCars?.map((car, index) => (
+                <CarCard key={`car-${index}`} car={car} />
               ))}
             </div>
 
             {loading && (
-              <div 
-                className='mt-16 w-full flex-center'
-              >
+              <div className='mt-16 w-full flex-center'>
                 <Image
-                  src='/loading.svg'
-                  alt='Loading'
+                  src='./loader.svg'
+                  alt='loader'
                   width={50}
                   height={50}
                   className='object-contain'
@@ -104,10 +96,12 @@ export default function Home() {
             />
           </section>
         ) : (
-          <div className='home__error-container'>
-            <h2 className='text-black text-xl font-bold'>Oops, no results</h2>
-            {/* <p>{allCars?.message}</p> */}
-          </div>
+          !loading && (
+            <div className='home__error-container'>
+              <h2 className='text-black text-xl font-bold'>Oops, no results</h2>
+              <p>{allCars?.message}</p>
+            </div>
+          )
         )}
       </div>
     </main>
