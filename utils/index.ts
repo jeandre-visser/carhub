26e02788b696c927c23859d7791a78837506a6af
Jeandre Visser <jeandre@nutrimeals.ca>
2023-06-20 21:02:36 -0600

    Move api keys to env file

--- utils/index.ts
+++ utils/index.ts
@@ -12,7 +12,7 @@ const options = {
 export async function fetchCars(filters: FilterProps) {
   const { manufacturer, limit, year, model, fuel } = filters;
   const headers = {
-    'X-RapidAPI-Key': 'f68de90bc2msh0afc2ba5c8e7022p11c4fejsn457033c3b522',
+    'X-RapidAPI-Key': process.env.NEXT_PUBLIC_RAPID_API_KEY || "",
     'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com',
   };
 
@@ -47,7 +47,7 @@ export const generateCarImageUrl = (car: CarProps, angle?: string) => {
   const url = new URL("https://cdn.imagin.studio/getimage");
   const { make, model, year } = car;
 
-  url.searchParams.append('customer', 'canutrimeals' || '');
+  url.searchParams.append('customer', process.env.NEXT_PUBLIC_IMAGIN_API_KEY || '');
   url.searchParams.append('make', make);
   url.searchParams.append('modelFamily', model.split(" ")[0]);
   url.searchParams.append('zoomType', 'fullscreen');
